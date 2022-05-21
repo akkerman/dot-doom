fb838244e6c6aab62b447f1f5febcac40a42b5c9
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2022-05-21 20:51:27 +0200

    Add more files to agenda

--- custom.el
+++ custom.el
@@ -4,10 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
-   '("~/org/slip-box/daily/2022-05-16.org" "/home/akkerman/org/slip-box/daily/2022-05-14.org" "/home/akkerman/org/slip-box/daily/2022-05-13.org" "/home/akkerman/org/gcal.org" "/home/akkerman/org/slip-box/20210430085309-luister_je_om_te_reageren_of_om_te_leren.org" "/home/akkerman/org/slip-box/20210227135359-javascript_in_emacs.org" "/home/akkerman/org/agenda.org" "/home/akkerman/org/feestdagen.org" "/home/akkerman/org/notes.org" "/home/akkerman/org/projects.org" "/home/akkerman/org/todo.org"))
+   '("~/org/slip-box/daily/2022-05-23.org" "/home/akkerman/org/slip-box/daily/2022-05-19.org" "/home/akkerman/org/slip-box/daily/2022-05-16.org" "/home/akkerman/org/slip-box/daily/2022-05-14.org" "/home/akkerman/org/slip-box/daily/2022-05-13.org" "/home/akkerman/org/gcal.org" "/home/akkerman/org/slip-box/20210430085309-luister_je_om_te_reageren_of_om_te_leren.org" "/home/akkerman/org/slip-box/20210227135359-javascript_in_emacs.org" "/home/akkerman/org/agenda.org" "/home/akkerman/org/feestdagen.org" "/home/akkerman/org/notes.org" "/home/akkerman/org/projects.org" "/home/akkerman/org/todo.org"))
  '(safe-local-variable-values
    '((org-roam-db-location . "~/Documents/mijn-aansluiting/org-roam.db")
-     (org-roam-directory . "~/Documents/mijn-aansluiting"))))
+     (org-roam-directory . "~/Documents/mijn-aansluiting")))
+ '(warning-suppress-types '((use-package) (use-package) (use-package) (use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
