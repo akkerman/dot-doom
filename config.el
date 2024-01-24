5fda10512da349fe70dc2fc21a3c0a4f61f6a8f1
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-01-24 20:30:09 +0100

    202401242030

--- config.el
+++ config.el
@@ -1,37 +1,5 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
-;; (setq user-full-name "John Doe"
-;;       user-mail-address "john@doe.com")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-symbol-font' -- for symbols
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
 ; (setq doom-theme 'doom-one)
 (setq doom-theme 'gruvbox-dark-medium)
 
@@ -39,9 +7,7 @@
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
-;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-
 (setq org-directory "~/org/")
 (after! org
   (setq org-hide-emphasis-markers t)
@@ -52,60 +18,45 @@
   (add-to-list 'org-capture-templates
                '("s" "Survival run Journal" entry
                  (file+datetree "slip-box/survival.org")
-                 (file "~/.doom.d/survival.template.org")))
+                 (file "~/.config/doom/survival.template.org")))
   (add-to-list 'org-capture-templates
                '("r" "Run Journal" entry
                  (file+datetree "slip-box/running.org")
-                (file "~/.doom.d/running.template.org"))))
+                (file "~/.config/doom/running.template.org"))))
 
 (setq org-roam-directory "~/org/slip-box")
 (after! org-roam
     (setq org-roam-capture-templates '(
         ("d" "default" plain "%?" :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n") :unnarrowed t)
         ("m" "mijn-aansluiting" plain "%?" :target (file+head "mijn-aansluiting/%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n") :unnarrowed t)
-        ("r" "refinement" plain (file "~/.doom.d/refinement.template.org") :target (file+head "mijn-aansluiting/%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n") :unnarrowed t)))
-    (setq org-roam-mode-section-functions
+        ("r" "refinement" plain (file "~/.config/doom/refinement.template.org") :target (file+head "mijn-aansluiting/%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n") :unnarrowed t)))
+    (setq org-roam-mode-sections
         (list #'org-roam-backlinks-section
               #'org-roam-reflinks-section
               #'org-roam-unlinked-references-section)))
 
 
+(defun viewsource/org-roam-node-from-cite (keys-entries)
+    (interactive (list (citar-select-ref :multiple nil :rebuild-cache t)))
+    (let ((title (citar--format-entry-no-widths (cdr keys-entries)
+                                                "${author editor}. (${date year}). ${title}")))
+      (org-roam-capture- :templates
+                         '(("r" "reference" plain (file "~/.config/doom/reference.template.org") :if-new
+                            (file+head "refs/${citekey}.org"
+                                       ":PROPERTIES:
+:ROAM_REFS: @${citekey}
+:END:
+#+title: ${title}\n")
+                         :immediate-finish t
+                         :unnarrowed t))
+                         :info (list :citekey (car keys-entries))
+                         :node (org-roam-node-create :title title)
+                         :props '(:finalize find-file))))
 
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;; To get information about any of these functions/macros, move the cursor over the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-
-
-;; Custom function
+;; Take screenshot
 (defun viewsource/org-scrot ()
   "Take a screenshot into a time stamped unique-named file in the
-same directory as the org-buffer and insert a link to this file."
+   same directory as the org-buffer and insert a link to this file."
   (interactive)
   (setq suffix (concat
                   "_scrot-"
@@ -120,5 +71,5 @@ same directory as the org-buffer and insert a link to this file."
 (setq doom-modeline-vcs-max-length 100)
 
 ;; Projectile
-(setq projectile-project-search-path '("~/git" "~/git/viewsource" "~/git/dsplatform" "~/git/creetion" "~/.config"))
+(setq projectile-project-search-path '("~/git/viewsource" "~/git/dsplatform" "~/git/creetion" "~/.config"))
 (setq ob-mermaid-cli-path "/home/akkerman/org/node_modules/.bin/mmdc")
