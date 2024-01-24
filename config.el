f40ed2da0fbc47173ac33eac0f1bf4a1d724dfdd
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-01-24 19:01:49 +0100

    Re-install doom

--- config.el
+++ config.el
@@ -5,38 +5,61 @@
 
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
-(setq user-full-name "Marcel Akkerman"
-      user-mail-address "akkerman@viewsource.nl")
+;; clients, file templates and snippets. It is optional.
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
 ;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
 ;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
+; (setq doom-theme 'doom-one)
 (setq doom-theme 'gruvbox-dark-medium)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+(setq org-roam-directory "~/org/slip-box")
 
-;; Here are some additional functions/macros that could help you configure Doom:
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
 ;;
 ;; - `load!' for loading external *.el files relative to this one
 ;; - `use-package!' for configuring packages
@@ -49,240 +72,8 @@
 ;; To get information about any of these functions/macros, move the cursor over
 ;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
 ;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
-
-;; Org-Roam
-(setq org-roam-directory "~/org/slip-box")
-;; (setq org-roam-directory "~/Documents/mijn-aansluiting")
-
-;; (setq
-;;       bibtex-completion-pdf-field "file"
-;;       bibtex-completion-bibliography '("~/org/bib/library.bib")
-;;       bibtex-completion-library-path '("~/Dropbox/Library/")
-;;       bibtex-completion-notes-path '("~/org/slip-box/refs")
-;;       )
-;;
-(setq org-roam-capture-templates
-      '(
-
-        ("d" "default" plain "%?" :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n") :unnarrowed t)
-        ("m" "mijn-aansluiting" plain "%?" :target (file+head "mijn-aansluiting/%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n") :unnarrowed t)
-        ("r" "refinement" plain (file "~/.doom.d/refinement.template.org") :target (file+head "mijn-aansluiting/%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n") :unnarrowed t)
-
-        ))
-
-(setq org-roam-mode-section-functions
-      (list #'org-roam-backlinks-section
-            #'org-roam-reflinks-section
-            #'org-roam-unlinked-references-section
-            ))
-
-(defun viewsource/org-roam-node-from-cite (keys-entries)
-    (interactive (list (citar-select-ref :multiple nil :rebuild-cache t)))
-    (let ((title (citar--format-entry-no-widths (cdr keys-entries)
-                                                "${author editor}. (${date year}). ${title}")))
-      (org-roam-capture- :templates
-                         '(("r" "reference" plain "%?" :if-new
-                            (file+head "refs/${citekey}.org"
-                                       ":PROPERTIES:
-:ROAM_REFS: @${citekey}
-:END:
-#+title: ${title}
-#+todo: READING(r) DEVELOP(D) | DONE(d) SKIP(s)
-#+setupfile: theme-readtheorg.setup
-#+filetags: :lit:
-#+startup: overview\n")
-                            :immediate-finish t
-                            :unnarrowed t))
-                         :info (list :citekey (car keys-entries))
-                         :node (org-roam-node-create :title title)
-                         :props '(:finalize find-file))))
-
-
-(defun viewsource/org-scrot ()
-  "Take a screenshot into a time stamped unique-named file in the
-same directory as the org-buffer and insert a link to this file."
-  (interactive)
-  (setq suffix (concat
-                  "_scrot-"
-                  (format-time-string "%Y%m%d%H%M%S") ".png"))
-  (setq absFilename (concat (buffer-file-name) suffix))
-  (setq relFilename (file-name-nondirectory absFilename))
-  (call-process "flameshot" nil nil nil "gui" "-p" absFilename)
-  (insert (concat "#+CAPTION: image-caption\n" "#+NAME:    fig:image-name\n" "[[./" relFilename "]]"))
-  (org-display-inline-images))
-
-;; Mode line
-(setq doom-modeline-vcs-max-length 100)
-
-;; Projectile
-(setq projectile-project-search-path '("~/git" "~/git/viewsource" "~/git/dsplatform" "~/git/creetion" "~/.config"))
-;; (setq projectile-create-missing-test-files t)
-
-(setq ob-mermaid-cli-path "/home/akkerman/org/node_modules/.bin/mmdc")
-
-
-
-;; https://github.com/hlissner/doom-emacs/tree/develop/modules/tools/biblio
-;; https://github.com/bdarcus/citar
-
-
-;; (setq! bibtex-completion-bibliography '("/home/akkerman/org/bib/library.bib"))
-;; (setq! bibtex-completion-library-path '("/home/akkerman/Dropbox/Library" "/home/akkerman/Dropbox/Library Bought")
-;;        bibtex-completion-notes-path "/home/akkerman/org/slip-box/refs")
-;; 
-
-(setq! citar-bibliography '("/home/akkerman/org/bib/library.bib"))
-(setq! citar-library-paths '("/home/akkerman/Dropbox/Library" "/home/akkerman/Dropbox/Library Bought")
-       citar-notes-paths '("/home/akkerman/org/slip-box/refs")
-       citar-org-roam-subdir "refs")
-
-
-;; fixing blank lines in org files
-(defun unpackaged/org-fix-blank-lines (&optional prefix)
-  "Ensure that blank lines exist between headings and between headings and their
-  contents. With prefix, operate on whole buffer. Ensures that blank lines exist
-  after each headings's drawers."  (interactive "P")
-  (org-map-entries (lambda ()
-                     (org-with-wide-buffer
-                      ;; `org-map-entries' narrows the buffer, which prevents us from seeing
-                      ;; newlines before the current heading, so we do this part widened.
-                      (while (not (looking-back "\n\n" nil))
-                        ;; Insert blank lines before heading.
-                        (insert "\n")))
-                     (let ((end (org-entry-end-position)))
-                       ;; Insert blank lines before entry content
-                       (forward-line)
-                       (while (and (org-at-planning-p)
-                                   (< (point) (point-max)))
-                         ;; Skip planning lines
-                         (forward-line))
-                       (while (re-search-forward org-drawer-regexp end t)
-                         ;; Skip drawers. You might think that `org-at-drawer-p' would suffice, but
-                         ;; for some reason it doesn't work correctly when operating on hidden text.
-                         ;; This works, taken from `org-agenda-get-some-entry-text'.
-                         (re-search-forward "^[ \t]*:END:.*\n?" end t)
-                         (goto-char (match-end 0)))
-                       (unless (or (= (point) (point-max))
-                                   (org-at-heading-p)
-                                   (looking-at-p "\n"))
-                         (insert "\n"))))
-                   t (if prefix
-                         nil
-                       'tree)))
-
-
-
-
-(after! org
-  (setq org-hide-emphasis-markers t)
-  (setq org-agenda-start-with-log-mode t)
-  (setq org-log-done 'time)
-  (setq org-log-into-drawer t)
-  (setq org-tags-column 75)
-  (add-to-list 'org-capture-templates
-               '("s" "Survival run Journal" entry
-                 (file+datetree "slip-box/survival.org")
-                 (file "~/.doom.d/survival.template.org")))
-  (add-to-list 'org-capture-templates
-               '("r" "Run Journal" entry
-                 (file+datetree "slip-box/running.org")
-                (file "~/.doom.d/running.template.org")))
-
-  ) ; after! org
-
-
-(setq org-roam-dailies-capture-templates
-      '(("d" "default" entry
-         "* %<%H:%M> %?"
-         :target (file+head "%<%Y-%m-%d>.org"
-                            "#+title: %<%Y-%m-%d>\n"))
-       ))
-
-(use-package! org-transclusion
-              :after org
-              :init
-;               (map!
-;                :map global-map "<f12>" #'org-transclusion-add
-;                :leader
-;                :prefix "n"
-;                :desc "Org Transclusion Mode" "t" #'org-transclusion-mode)
-               )
-
-;; automatically fix javascript errors
-(setq eslint-fix-executable "eslint_d")
-(eval-after-load 'js2-mode
-  '(add-hook 'js2-mode-hook (lambda () (add-hook 'after-save-hook 'eslint-fix nil t))))
-
-(map! :leader
-      :desc "Toggle impl and test"
-      "p v"
-        #'projectile-toggle-between-implementation-and-test)
-
-
-(map! :leader
-      :desc "Split, toggle impl and test"
-      "w a"
-      (cmd!
-       (evil-window-vsplit)
-       (projectile-toggle-between-implementation-and-test)))
-
-(map! :leader
-      :desc "Split, toggle impl and test"
-      "w i"
-      (cmd!
-       (evil-window-split)
-       (evil-edit "index.js")))
-
-(map! :desc "Next error" :n "] g" #'flycheck-next-error)
-(map! :desc "Prev error" :n "[ g" #'flycheck-previous-error)
-
-;; (use-package! 'org-gcal)
-;; (setq org-gcal-client-id "902740636832-kpb8amtvh453m2oo2p2t9lr7ml3pic0c.apps.googleusercontent.com"
-;;       org-gcal-client-secret "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"
-;;       org-gcal-fetch-file-alist '(("marcel.akkerman@gmail.com" .  "~/org/gcal.org")
-;;                                   ))
-
-(setq org-agenda-start-on-weekday 1)
-
-(setq org-latex-toc-command "\\tableofcontents \\clearpage")
-
-  (defun my/org-roam-swap-work-and-home ()
-    "Swap between home and work org roam databases."
-    (interactive)
-    (if (equal org-roam-directory "~/org")
-        (setq org-roam-directory "~/Documents/mijn-aansluiting"
-              org-roam-db-location "~/Document/mijn-aansluiting/org-roam.db")
-      (setq org-roam-directory "~/org"
-            org-roam-db-location "~/.config/emacs/.local/cache/org-roam.db"))
-    (org-roam-db-sync))
-
-
-;; https://emacs.stackexchange.com/questions/12613/convert-the-first-character-to-uppercase-capital-letter-using-yasnippet
-(defun my/capitalize-first-char (&optional string)
-  "Capitalize only the first character of the input STRING."
-  (when (and string (> (length string) 0))
-    (let ((first-char (substring string nil 1))
-          (rest-str   (substring string 1)))
-      (concat (capitalize first-char) rest-str))))
-
-
-(fset 'BLI\ header
-   (kmacro [?0 ?y ?t ?  ?m ?m ?? ?\C-r ?\" return ?d ?d ?\' ?m ?p ?k ?  ?m ?h ?k] 0 "%d"))
-
-
-;; change default mode for javascript files (was rsjx)
-(add-to-list 'auto-mode-alist '("\\.[mc]?js\\'" . typescript-mode))
-
-
-;; (add-to-list 'hl-todo-keyword-faces
-;;              '(("it.todo" warning bold)
-;;                ("it.skip" warning bold))
-
-(after! magit-todos
-  (setq magit-todos-keywords-list '("TODO" "FIXME" "DEPRECATED" "BUG" "it.todo" "it.skip")))
-
-(add-hook 'typescript-mode-hook 'eslint-fix-auto-mode)
-(add-hook 'typescript-mode-hook 'lsp)
--- init.el
+++ init.el
@@ -4,8 +4,8 @@
 ;; in. Remember to run 'doom sync' after modifying it!
 
 ;; NOTE Press 'SPC h d h' (or 'C-h d h' for non-vim users) to access Doom's
-;;      documentation. There you'll find a "Module Index" link where you'll find
-;;      a comprehensive list of Doom's modules and what flags they support.
+;;      documentation. There you'll find a link to Doom's Module Index where all
+;;      of our modules are listed, including what flags they support.
 
 ;; NOTE Move your cursor over a module's name (or its flags) and press 'K' (or
 ;;      'C-c c k' for non-vim users) to view its documentation. This works on
@@ -15,6 +15,7 @@
 ;;      directory (for easy access to its source code).
 
 (doom! :input
+       ;;bidi              ; (tfel ot) thgir etirw uoy gnipleh
        ;;chinese
        ;;japanese
        ;;layout            ; auie,ctsrnm is the superior home row
@@ -23,14 +24,14 @@
        company           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       (ivy)              ; a search engine for love and life
+       ;;ivy               ; a search engine for love and life
        vertico           ; the search engine of the future
 
        :ui
        ;;deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
-       doom-quit         ; DOOM quit-message prompts when you quit Emacs
+       ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
        ;;(emoji +unicode)  ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
@@ -45,7 +46,7 @@
        ;;tabs              ; a tab bar for Emacs
        ;;treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
-       vc-gutter         ; vcs diff in the fringe
+       (vc-gutter +pretty) ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
@@ -73,10 +74,10 @@
        vc                ; version-control and Emacs, sitting in a tree
 
        :term
-       eshell            ; the elisp shell that works everywhere
-       shell             ; simple shell REPL for Emacs
-       term              ; basic terminal emulator for Emacs
-       ;; vterm             ; the best terminal emulation in Emacs
+       ;;eshell            ; the elisp shell that works everywhere
+       ;;shell             ; simple shell REPL for Emacs
+       ;;term              ; basic terminal emulator for Emacs
+       ;;vterm             ; the best terminal emulation in Emacs
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
@@ -85,25 +86,27 @@
 
        :tools
        ;;ansible
-       biblio            ; Writes a PhD for you (citation needed)
+       ;;biblio            ; Writes a PhD for you (citation needed)
+       ;;collab            ; buffers with friends
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
-       docker
+       ;;docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
-       lsp               ; M-x vscode
+       ;;lsp               ; M-x vscode
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
-       pdf               ; pdf enhancements
+       ;;pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        ;;rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
-       terraform         ; infrastructure as code
+       ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
+       ;;tree-sitter       ; syntax and parsing, sitting in a tree...
        ;;upload            ; map local to remote projects via ssh/ftp
 
        :os
@@ -113,7 +116,7 @@
        :lang
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
-       ;;cc                ; C > C++ == 1
+       ;;(cc +lsp)         ; C > C++ == 1
        ;;clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
@@ -134,12 +137,13 @@
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;gdscript          ; the language you waited for
        ;;(go +lsp)         ; the hipster dialect
-       (haskell +lsp)    ; a language that's lazier than I am
+       ;;(graphql +lsp)    ; Give queries a REST
+       ;;(haskell +lsp)    ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
-       json              ; At least it ain't XML
-       ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
-       javascript        ; all(hope(abandon(ye(who(enter(here))))))
+       ;;json              ; At least it ain't XML
+       ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
+       ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        ;;latex             ; writing papers in Emacs has never been so fun
@@ -150,18 +154,18 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +froam2 +pretty +noter)        ; organize your plain life in plain text
+       (org +roam2 +pretty +noter) ;; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       python           ; beautiful is better than ugly
+       ;;python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       ;;(rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
@@ -170,7 +174,7 @@
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        ;;web               ; the tubes
-       yaml              ; JSON, but readable
+       ;;yaml              ; JSON, but readable
        ;;zig               ; C, but simpler
 
        :email
