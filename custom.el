ee11856a894cc11ac521b971c008d0215d7e95ea
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-03-11 07:43:11 +0100

    Add a roam files to org-agenda

--- custom.el
+++ custom.el
@@ -4,8 +4,14 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(org-agenda-files
+   '("~/org/slip-box/running.org" "/home/akkerman/org/slip-box/20240116124311-hoogbegaafdheid.org" "/home/akkerman/org/slip-box/20230313090519-verbouwing_2023.org" "/home/akkerman/org/slip-box/20220929053741-current_projects.org" "/home/akkerman/org/slip-box/20210228143401-python_virtualenv.org" "/home/akkerman/org/slip-box/20210227135359-javascript_in_emacs.org" "/home/akkerman/org/slip-box/20230413084910-what_is_head_during_merge_or_rebase.org" "/home/akkerman/org/slip-box/mijn-aansluiting/20230522092621-uitfaseren_en_vervangen_auth0_rules.org" "/home/akkerman/org/slip-box/mijn-aansluiting/20240307154655-overgang_lip.org" "/home/akkerman/Downloads/Overgang LIP.org" "/home/akkerman/org/agenda.org" "/home/akkerman/org/feestdagen.org" "/home/akkerman/org/habits.org" "/home/akkerman/org/journal.org" "/home/akkerman/org/notes.org" "/home/akkerman/org/projects.org" "/home/akkerman/org/todo.org"))
+ '(org-safe-remote-resources
+   '("\\`https://fniessen\\.github\\.io/org-html-themes/org/theme-readtheorg\\.setup\\'"))
  '(package-selected-packages '(eslintd-fix))
- '(safe-local-variable-values '((projectile-project-name . "CAPO API"))))
+ '(safe-local-variable-values
+   '((projectile-project-name . "Beheer API")
+     (projectile-project-name . "CAPO API"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
