a6b4af30a8c01f84a1ea87480a65958fadc08812
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-03-11 13:58:38 +0100

    Configure calendar
    start on monday
    add weeknumber

--- config.el
+++ config.el
@@ -13,6 +13,21 @@
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
+;; calendar
+(setq calendar-week-start-day 1)
+
+
+;; https://www.emacswiki.org/emacs/CalendarWeekNumbers
+(copy-face font-lock-constant-face 'calendar-iso-week-face)
+(set-face-attribute 'calendar-iso-week-face nil :height 0.7)
+(setq calendar-intermonth-text
+      '(propertize
+        (format "%2d"
+                (car
+                 (calendar-iso-from-absolute
+                  (calendar-absolute-from-gregorian (list month day year)))))
+        'font-lock-face 'calendar-iso-week-face))
+
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 (after! org
