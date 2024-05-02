97d01fbf1bda9bdb61c0ab11acceedd733d5f8b5
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-05-02 15:29:44 +0200

    Update agenda files

--- custom.el
+++ custom.el
@@ -5,13 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(org-agenda-files
-   '("/home/akkerman/org/slip-box/20240314142153-security_meeting.org" "/home/akkerman/org/tickler.org" "/home/akkerman/org/slip-box/mijn-aansluiting/20240318090837-opdrachtverstrekking_early_adoption_dsp_2_0.org" "/home/akkerman/org/slip-box/20240311163003-journaling.org" "/home/akkerman/org/slip-box/running.org" "/home/akkerman/org/slip-box/20240116124311-hoogbegaafdheid.org" "/home/akkerman/org/slip-box/20210228143401-python_virtualenv.org" "/home/akkerman/org/slip-box/20210227135359-javascript_in_emacs.org" "/home/akkerman/org/slip-box/20230413084910-what_is_head_during_merge_or_rebase.org" "/home/akkerman/org/slip-box/mijn-aansluiting/20230522092621-uitfaseren_en_vervangen_auth0_rules.org" "/home/akkerman/org/slip-box/mijn-aansluiting/20240307154655-overgang_lip.org" "/home/akkerman/org/agenda.org" "/home/akkerman/org/feestdagen.org" "/home/akkerman/org/habits.org" "/home/akkerman/org/notes.org" "/home/akkerman/org/projects.org" "/home/akkerman/org/todo.org"))
- '(org-safe-remote-resources
-   '("\\`https://fniessen\\.github\\.io/org-html-themes/org/theme-readtheorg\\.setup\\'"))
- '(package-selected-packages '(eslintd-fix))
- '(safe-local-variable-values
-   '((projectile-project-name . "Beheer API")
-     (projectile-project-name . "CAPO API"))))
+   '("~/org/slip-box/running.org" "/home/akkerman/org/feestdagen.org" "/home/akkerman/org/projects.org" "/home/akkerman/org/tickler.org" "/home/akkerman/org/todo.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
