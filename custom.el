3dc0a9e8a821e8b62ae69219ac7cb30e2b562856
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2025-08-19 07:41:33 +0200

    202508190741

--- custom.el
+++ custom.el
@@ -3,9 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("b7a09eb77a1e9b98cafba8ef1bd58871f91958538f6671b22976ea38c2580755" default))
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(org-agenda-files
-   '("~/org/slip-box/mijn-aansluiting/20240506095251-verbeteringen_scheduler.org" "/home/akkerman/org/slip-box/running.org" "/home/akkerman/org/feestdagen.org" "/home/akkerman/org/projects.org" "/home/akkerman/org/tickler.org" "/home/akkerman/org/todo.org"))
+   '("~/org/slip-box/mijn-aansluiting/20240506095251-verbeteringen_scheduler.org"
+     "/home/akkerman/org/slip-box/running.org"
+     "/home/akkerman/org/feestdagen.org" "/home/akkerman/org/projects.org"
+     "/home/akkerman/org/tickler.org" "/home/akkerman/org/todo.org"))
  '(safe-local-variable-values '((projectile-project-name . "CAPO API"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
