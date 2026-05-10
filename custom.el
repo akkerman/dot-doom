0409a3ba29a1992d0ee2a13c8a7687e2861cfe33
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2026-05-10 16:40:44 +0200

    config updates

--- custom.el
+++ custom.el
@@ -7,11 +7,13 @@
    '("b7a09eb77a1e9b98cafba8ef1bd58871f91958538f6671b22976ea38c2580755" default))
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(org-agenda-files
-   '("~/org/slip-box/20210227135359-javascript_in_emacs.org"
-     "/home/akkerman/org/slip-box/running.org"
-     "/home/akkerman/org/feestdagen.org" "/home/akkerman/org/projects.org"
-     "/home/akkerman/org/tickler.org" "/home/akkerman/org/todo.org"))
- '(safe-local-variable-values '((projectile-project-name . "CAPO API"))))
+   '("/home/akkerman/org/slip-box/running.org" "/home/akkerman/org/feestdagen.org"
+     "/home/akkerman/org/projects.org" "/home/akkerman/org/tickler.org"
+     "/home/akkerman/org/todo.org"))
+ '(safe-local-variable-values
+   '((projectile-project-name . capo-review)
+     (projectile-project-name . capo-feature)
+     (projectile-project-name . "CAPO API"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
